7cb2664f6c31f805fcf810cba474e10cdb65683d
aoneban <torgregion1@gmail.com>
2023-04-15 13:46:57 +0200

    feat: add new solve

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,33 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (Number(sampleActivity) > 0 && Number(sampleActivity) < 15) {
+    if (isNaN(Number(sampleActivity))) {
+      return false;
+    } else if (
+      sampleActivity === '' ||
+      sampleActivity === ' ' ||
+      sampleActivity === ' \n\t\r'
+    ) {
+      return false;
+    } else if (
+      typeof sampleActivity === 'number' ||
+      typeof sampleActivity === 'boolean' ||
+      typeof sampleActivity === 'object' ||
+      typeof sampleActivity === 'undefined' ||
+      typeof sampleActivity === null
+    ) {
+      return false;
+    } else {
+      const constYears = 0.693 / 5730;
+      const start = 15;
+      const log = Math.log(start / sampleActivity);
+      return Math.ceil(log / constYears);
+    }
+  } else {
+    return false
+  }
 }
 
 module.exports = {
