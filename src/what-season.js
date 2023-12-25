d6f8c10e58a2690fed57e15ef63dc45ccfe25bf9
aoneban <torgregion1@gmail.com>
2023-12-25 20:19:44 +0100

    feat: add full solution to getSeason task

--- src/what-season.js
+++ src/what-season.js
@@ -15,26 +15,31 @@ function getSeason(date) {
   function isValidDate(dates) {
     return (dates && Object.prototype.toString.call(dates) === '[object Date]' && !isNaN(dates));
   }
-  if (isValidDate(date) === false) {
-    throw new Error('Invalid date!');
-  } else {
-    if (typeof date === 'undefined') {
-      return 'Unable to determine the time of year!';
+  try {
+    if (isValidDate(date) === false) {
+      throw new Error('Invalid date!');
     } else {
-      let month = date.getMonth();
-      if (month >= 0 && month < 2) {
-        return 'winter';
-      } else if (month >= 2 && month < 5) {
-        return 'spring';
-      } else if (month >= 5 && month < 8) {
-        return 'summer';
-      } else if (month >= 8 && month < 11) {
-        return 'autumn';
-      } else if (month === 11) {
-        return 'winter';
+      if (typeof date === 'undefined') {
+        return 'Unable to determine the time of year!';
+      } else {
+        let month = date.getMonth();
+        if (month >= 0 && month < 2) {
+          return 'winter';
+        } else if (month >= 2 && month < 5) {
+          return 'spring';
+        } else if (month >= 5 && month < 8) {
+          return 'summer';
+        } else if (month >= 8 && month < 11) {
+          return 'autumn';
+        } else if (month === 11) {
+          return 'winter';
+        }
       }
     }
+  } catch (e) {
+    throw new Error('Invalid date!');
   }
+  
 }
 
 module.exports = {
