6525b261c97d8b1e8942e10de02f9ae0185fa705
aoneban <torgregion1@gmail.com>
2025-01-05 12:35:20 +0100

    fix: fix solution by repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,28 +2,40 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
 function repeater(str, options) {
+  const repeatTimes =
+    options.repeatTimes !== undefined ? options.repeatTimes : 1;
+  const separator = options.separator !== undefined ? options.separator : '+';
+  const addition = options.addition !== undefined ? options.addition : '';
+  const additionRepeatTimes =
+    options.additionRepeatTimes !== undefined ? options.additionRepeatTimes : 1;
+  const additionSeparator =
+    options.additionSeparator !== undefined ? options.additionSeparator : '|';
 
-  const x = options.additionSeparator.length;
-  const y = options.separator.length;
-  const str1 = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes).slice(0, -x);
-  const result = (str + str1 + options.separator).repeat(options.repeatTimes).slice(0, -y);
-  return result
+  const x = additionSeparator.length;
+  const y = separator.length;
+  const str1 = (addition + additionSeparator)
+    .repeat(additionRepeatTimes)
+    .slice(0, -x);
+  const result = (str + str1 + separator)
+    .repeat(repeatTimes)
+    .slice(0, -y);
+  return result;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
